6245e8a07e054ddba4a6e9aacf10b1f753eb76c6
Satish Raj Janakiraman <satish.janakiraman@ros.com>
2024-12-05 20:16:47 -0700

    adjust the assets for the relative path

--- src/components/three/commons/celestial/Moon.tsx
+++ src/components/three/commons/celestial/Moon.tsx
@@ -2,7 +2,7 @@ import { useGLTF } from "@react-three/drei";
 import * as THREE from 'three';
 
 const Moon: React.FC<{ rotationRef: React.MutableRefObject<THREE.Group | null> }> = ({ rotationRef }) => {
-    const { scene } = useGLTF('/assets/moon.glb');
+    const { scene } = useGLTF('/falcon/assets/moon.glb');
     return <primitive ref={rotationRef} object={scene} />;
 };
 
